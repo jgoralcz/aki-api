8816658b7a14eeb0b4a2b13ac325fe69f0e2a7a1
Joshua Goralczyk <joshgor01@gmail.com>
2021-01-05 22:24:16 -0700

    add publish github action and update axios dependency

--- src/lib/functions/Request.js
+++ src/lib/functions/Request.js
@@ -1,10 +1,5 @@
 const axios = require('axios');
 
-/**
- * requests for a uri with default options.
- * @param uri the user's uri.
- * @returns {Promise<void>}
- */
 const headers = {
   Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
   'Accept-Encoding': 'gzip, deflate',
@@ -13,18 +8,15 @@ const headers = {
   'x-requested-with': 'XMLHttpRequest',
 };
 
-const params = {
+const params = Object.freeze({
   gzip: true,
   resolveWithFullResponse: true,
   timeout: 10000,
-};
+});
 
 const rp = async (uri) => {
   try {
-    const result = await axios.get(uri, {
-      headers,
-      params,
-    });
+    const result = await axios.get(uri, { headers, params });
     const beginningParse = result.data.indexOf('(');
     const jsonString = result.data.substring(beginningParse + 1, result.data.length - 1);
     result.data = JSON.parse(jsonString);
